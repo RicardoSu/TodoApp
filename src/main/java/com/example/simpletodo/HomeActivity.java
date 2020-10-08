2d4e43ad4f0addc0500dce5b4fd82a0636ac5ffb
Ricardo Su <ricardolimasuarez@gmail.com>
2020-10-07 21:11:08 -0400

    splash screen

---
+++ src/main/java/com/example/simpletodo/HomeActivity.java
@@ -0,0 +1,29 @@
+package com.example.simpletodo;
+
+import android.content.Intent;
+import android.os.Handler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i= new Intent(HomeActivity.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+
+    }
+}
